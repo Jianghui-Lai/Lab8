ecbe5c11aeee32be3b5aa0076702700ab029e905
Jianghui-Lai <laijianghui@gmail.com>
2023-03-07 17:44:45 -0700

    RED: add test for add city

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -1,16 +1,27 @@
 package com.example.lab8;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
+//import com.example.lab8;
+import com.example.lab8.City;
+import com.example.lab8.CustomList;
+
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
+    private ArrayList<City> citylist = new ArrayList<>();
 
-
+    @Test
+    public void testAddcity() {
+        CustomList list = new CustomList(null, citylist);
+        City city = new City("Edmonton", "AB");
+        list.addCity(city);
+        assertEquals(1, list.getCount());
+    }
 }
